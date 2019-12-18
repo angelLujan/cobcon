ea91106814a1525a354ed132f68450dbd3997bda
joseKipup <angel.lujan@kipup.mx>
2019-12-18 15:28:16 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,351 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>COBCON</title>
+    <link rel="icon" href="img/icon.png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-transparent">
+    <div class="container">
+        <a class="navbar-brand" href="#banner"><img src="img/logo.png" alt="COBCON"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#nosotros">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#servicios">Servicios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#proyectos">Proyectos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#testimoniales">Testimoniales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contacto">Contacto</a>
+                </li>
+                <li class="nav-item" id="social">
+                    <a class="nav-link" href="#" target="_blank"><img src="img/social/fb.png" alt="Facebook"></a>
+                    <a class="nav-link" href="#" target="_blank"><img src="img/social/ins.png" alt="Instagram"></a>
+                    <a class="nav-link" href="#" target="_blank"><img src="img/social/wp.png" alt="WhatsApp"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="section" id="banner">
+    <div class="container">
+        <div id="carouselBanner" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators pl-4 pr-4">
+                <li data-target="#carouselBanner" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselBanner" data-slide-to="1"></li>
+                <li data-target="#carouselBanner" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner vertical-align-abs p-4">
+                <div class="carousel-item w-75 active">
+                    <h1>Especialistas en el servicio de techado desde 1980</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <button class="btn">Cotiza hoy mismo</button>
+                </div>
+                <div class="carousel-item w-75">
+                    <h1>Especialistas en el servicio de techado desde 1980</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <button class="btn">Cotiza hoy mismo</button>
+                </div>
+                <div class="carousel-item w-75">
+                    <h1>Especialistas en el servicio de techado desde 1980</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <button class="btn">Cotiza hoy mismo</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section pb-5 position-relative" id="nosotros">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-7 col-sm-8 offset-lg-6 offset-md-5 offset-sm-4 p-5">
+                <h3 class="section-title light-title">Acerca de<br>Nosotros</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco aboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco aboris .
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section pt-5" id="servicios">
+    <div class="container">
+        <div class="row align-items-center pb-5 mb-5">
+            <div class="col-lg-3">
+                <h3 class="section-title dark-title">Conoce nuestros<br>Servicios</h3>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco aboris nisi ut
+                    aliquip
+                    ex ea commodo consequat.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-4 col-4">
+                <div class="img-bg">
+                    <p class="img-text">Lorem ipsum</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-4 col-4">
+                <div class="img-bg">
+                    <p class="img-text">Lorem ipsum</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-4 col-4">
+                <div class="img-bg">
+                    <p class="img-text">Lorem ipsum</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="proyectos" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-4 p-0"><img src="img/proyectos/1.png" class="d-block w-100" alt="..."></div>
+                        <div class="col-4 p-0"><img src="img/proyectos/2.png" class="d-block w-100" alt="..."></div>
+                        <div class="col-4 p-0"><img src="img/proyectos/3.png" class="d-block w-100" alt="..."></div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-4 p-0"><img src="img/proyectos/1.png" class="d-block w-100" alt="..."></div>
+                        <div class="col-4 p-0"><img src="img/proyectos/2.png" class="d-block w-100" alt="..."></div>
+                        <div class="col-4 p-0"><img src="img/proyectos/3.png" class="d-block w-100" alt="..."></div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#proyectos" role="button" data-slide="prev">
+                <i class="far fa-arrow-alt-circle-left"></i>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#proyectos" role="button" data-slide="next">
+                <i class="far fa-arrow-alt-circle-right"></i>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="section pt-5 pb-5 text-center" id="testimoniales">
+    <div class="container mb-5">
+        <h3 class="section-title light-title mt-5 pt-5">Sobre nuestros<br>Testimoniales</h3>
+        <div id="carouselTestimony" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco aboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <h3>John Smith</h3>
+                        </div>
+                        <div class="col-sm-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco aboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <h3>John Smith</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco aboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <h3>John Smith</h3>
+                        </div>
+                        <div class="col-sm-6">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et doloremagna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco aboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                            <h3>John Smith</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section" id="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1750.8229279678017!2d-106.07280317844445!3d28.640374319769485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDM4JzI1LjMiTiAxMDbCsDA0JzE5LjkiVw!5e0!3m2!1sen!2smx!4v1576702219024!5m2!1sen!2smx" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
+<div class="section pt-5 pb-5" id="contacto">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-6">
+                <h3 class="section-title dark-title">¿Dudas?<br>¡Escríbenos!</h3>
+                <form action="">
+                    <input type="text" name="name" placeholder="Nombre">
+                    <input type="tel" name="phone" placeholder="Teléfono">
+                    <input type="email" name="email" placeholder="Correo electrónico">
+                    <textarea name="msg"  cols="30" rows="3" placeholder="Mensaje"></textarea>
+                    <button class="btn text-center">Enviar</button>
+                </form>
+            </div>
+            <div class="col-md-5 col-sm-6 offset-md-2">
+                <h3 class="section-title light-title">Visítanos<br>¡Te esperamos!</h3>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>
+                            Lorem Ipsum #1234<br>
+                            Col. Lorem Ipsum C.P 12345<br>
+                            Chihuahua, Chih
+                        </p>
+                        <p>
+                            Tel. +52 (614)123 4567<br>
+                            Tel. +52 (614)123 4567
+                        </p>
+                        <p>
+                            <a href="mailto:hola@cobcon.com">hola@cobcon.com</a>
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p>
+                            Lorem Ipsum #1234<br>
+                            Col. Lorem Ipsum C.P 12345<br>
+                            Chihuahua, Chih
+                        </p>
+                        <p>
+                            Tel. +52 (614)123 4567<br>
+                            Tel. +52 (614)123 4567
+                        </p>
+                        <p>
+                            <a href="mailto:hola@cobcon.com">hola@cobcon.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="" id="footer-top">
+        <div class="container text-center position-relative">
+            <img src="img/logo.png" alt="COBCON">
+            <span class="vertical-align-abs" id="social">
+                <a class="d-inline-block" href="#" target="_blank"><img src="img/social/fb.png" alt="Facebook"></a>
+                <a class="d-inline-block" href="#" target="_blank"><img src="img/social/ins.png" alt="Instagram"></a>
+                <a class="d-inline-block" href="#" target="_blank"><img src="img/social/wp.png" alt="WhatsApp"></a>
+            </span>
+        </div>
+    </div>
+    <div class="text-center pt-3 pb-3" id="footer-bottom">
+        <span>Derechos Reservados <?php echo date('Y'); ?></span>
+    </div>
+</footer>
+
+<script>
+    (() => {
+        $('.navbar-nav .nav-item').click(function () {
+            let item = $(this);
+            if (!item.is(':last-child')) {
+                $('.navbar-nav .nav-item').removeClass('active');
+                item.addClass('active');
+            }
+        });
+
+        let checkScrollTop = () => {
+            if ($(window).scrollTop() > 86)
+                $('.navbar').addClass('navbar-scroll');
+            if ($(window).scrollTop() < 87)
+                $('.navbar').removeClass('navbar-scroll');
+        };
+        $(window).scroll(function () {
+            //if you hard code, then use console
+            //.log to determine when you want the
+            //nav bar to stick.
+            checkScrollTop();
+        });
+        checkScrollTop();
+
+        /*
+        Smooth Scroll
+        */
+        // Select all links with hashes
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (event) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+                    &&
+                    location.hostname === this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        event.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            }
+                        });
+                    }
+                }
+            });
+    })();
+</script>
+</body>
+</html>
